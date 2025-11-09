7e4e60f6d18bf908328b4b46345838f738acf099
Yashvi Reddy <syashvireddy@gmail.com>
2025-11-09 23:41:24 +0530

    edited

--- terraform/main.tf
+++ terraform/main.tf
@@ -114,17 +114,4 @@ resource "aws_iam_user_policy" "jenkins_policy" {
   })
 }
 
-# -------------------------------
-# Terraform Outputs
-# -------------------------------
-output "s3_bucket_name" {
-  value = aws_s3_bucket.report_bucket.bucket
-}
 
-output "s3_url" {
-  value = "https://${aws_s3_bucket.report_bucket.bucket}.s3.amazonaws.com/report.html"
-}
-
-output "cloudfront_url" {
-  value = "https://${aws_cloudfront_distribution.report_distribution.domain_name}/report.html"
-}
